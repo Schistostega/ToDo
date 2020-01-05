37f60aeb00d0702a8fdd131bac561f691ce43ea9
Schistostega <[No data]>
2020-01-05 17:20:54 +0200

    add time zone support

--- ToDo/desk/urls.py
+++ ToDo/desk/urls.py
@@ -6,4 +6,5 @@ app_name = 'desk'
 urlpatterns = [
     path('projects', views.ProjectListView.as_view(), name='index'),
     path('projects/<int:pk>', views.ProjectUpdateView.as_view(), name='project_detail'),
+    path('set-timezone', views.set_timezone, name='set_timezone'),
 ]
--- ToDo/desk/views.py
+++ ToDo/desk/views.py
@@ -1,5 +1,15 @@
+import pytz
 from django.views import generic
+from django.shortcuts import render, redirect
 from .models import Project
+
+
+def set_timezone(request):
+    if request.method == 'POST':
+        request.session['django_timezone'] = request.POST['timezone']
+        return redirect('/desk/projects')  # !!!HARDCODE!!!
+    else:
+        return render(request, 'desk/tz_template.html', {'timezones': pytz.common_timezones})
 
 
 class ProjectListView(generic.ListView):
